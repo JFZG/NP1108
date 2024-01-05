789e9f7d779e902117612f3a6832d2b29cb399e3
JFZG <91310549+JFZG@users.noreply.github.com>
2024-01-06 01:24:07 +0800

    0106-3

--- Assets/Scripts/TriggerSystem.cs
+++ Assets/Scripts/TriggerSystem.cs
@@ -7,6 +7,18 @@ namespace JJF
     ///</summary>
     public class TriggerSystem : MonoBehaviour
     {
- 
+        [SerializeField, Header("要顯示的物件")]
+        private GameObject objectToShow;
+
+
+       private void OnTriggerEnter(Collider other)
+        {
+            if (other.name.Contains("Player"))
+            {
+                print("玩家進入觸發區域");
+                objectToShow.SetActive(true);
+            }
+           
+        }
     }
 }
